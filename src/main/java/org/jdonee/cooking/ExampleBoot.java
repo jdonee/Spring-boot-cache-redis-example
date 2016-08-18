267ad5002d388c142cf6b09f43f3c038792d23fe
Frank.Zeng <Frank.Zeng@DESKTOP-K0CSG8O>
2016-08-18 15:25:27 +0800

    1、更新忽略列表

--- src/main/java/org/jdonee/cooking/ExampleBoot.java
+++ src/main/java/org/jdonee/cooking/ExampleBoot.java
@@ -12,6 +12,16 @@ import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * 1、Spring boot + Spring Cache +Spring Data Redis+Spring AOP。
+ * 2、接受Spring Cache的普通用法。
+ * 3、使用Jackson2存储Redis缓存对象。
+ * 4、使用AOP处理当对象保存、更新或删除时对象列表的更新。
+ * 5、对象列表更新使用Redis的订阅异步处理。
+ * 
+ * @author Frank.Zeng
+ *
+ */
 @SpringBootApplication
 @Slf4j
 public class ExampleBoot {
